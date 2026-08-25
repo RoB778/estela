7bb5c61424f161d3479b3b3e71d91c5de2e8a2ba
RoB778 <140019223+RoB778@users.noreply.github.com>
2026-08-25 12:50:56 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import ArticuloDetalle from './ArticuloDetalle.jsx'
 import './index.css'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
